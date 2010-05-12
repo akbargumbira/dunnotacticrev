aada0211aaa20c2553b72e924cb7a040da75245e
rezanachmad@gmail.com <rezanachmad@gmail.com@daeb38db-4d48-a229-e774-feb0693dbecc>
2010-05-12 13:29:09 +0000

    Pertamax!!!

---
+++ trunk/DunnoTactic/src/Model/Character/Job.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model.Character;
+
+/**
+ *
+ * @author user
+ */
+public class Job {
+    protected  int JobID;
+    protected  int JobClass;
+    protected  float[] FactorJob = new float[Constanta.NUMBER_ATRIBUT];
+    
+}
---
+++ trunk/DunnoTactic/src/Support/Converter.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Support;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author user
+ */
+public class Converter {
+    public static Point PointToGrid(int x, int y) {
+        int X = x/ImageSupport.IMAGE_WIDTH;
+        int Y = y/ImageSupport.IMAGE_HEIGHT;
+        return new Point(X, Y);
+    }
+
+    public static Point PointToGrid(Point point) {
+        return PointToGrid(point.x, point.y);
+    }
+
+    public static Point GridToPoint(int x, int y) {
+        int X = x*ImageSupport.IMAGE_WIDTH;
+        int Y = y*ImageSupport.IMAGE_HEIGHT;
+        return new Point(X, Y);
+    }
+
+    public static Point GridToPoint(Point point) {
+        return GridToPoint(point.x, point.y);
+    }
+}
