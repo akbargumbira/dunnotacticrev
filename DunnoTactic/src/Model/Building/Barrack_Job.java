5844ce79299e9b2b099850d5f8fb204b313f43fd
gumbira.mymind <gumbira.mymind@daeb38db-4d48-a229-e774-feb0693dbecc>
2010-05-14 06:37:25 +0000

    Building udah agak lengkap

--- DunnoTactic/src/Model/Building/Barrack_Job.java
+++ DunnoTactic/src/Model/Building/Barrack_Job.java
@@ -11,12 +11,21 @@ package Model.Building;
  */
 public class Barrack_Job extends Building
 {
+   public static final int COST =100;
    public Barrack_Job()
    {
-       Building_BaseAtribut[Building_Constanta.BUILDING_HP_ID] = 4000;
-       Building_BaseAtribut[Building_Constanta.BUILDING_DEFENSE_ID] = 200;
-       Building_BaseAtribut[Building_Constanta.BUILDING_BUILD_TIME_ID] = 2;
-       Building_BaseAtribut[Building_Constanta.BUILDING_GOLD_PER_TURN_ID] = 50;
+       Building_BaseAtribut[BUILDING_HP_ID] = 25000;
+       Building_BaseAtribut[BUILDING_DEFENSE_ID] = 200;
+       Building_BaseAtribut[BUILDING_BUILD_TIME_ID] = 2;
+       Building_BaseAtribut[BUILDING_GOLD_PER_TURN_ID] = 0;
+       Building_BaseAtribut[BUILDING_LEVEL_ID]=1;
    }
 
+    public void PerfectBuilding()
+    {
+        Building_BaseAtribut[BUILDING_HP_ID]=2*Building_BaseAtribut[BUILDING_HP_ID];
+        Building_BaseAtribut[BUILDING_DEFENSE_ID] = 2*Building_BaseAtribut[BUILDING_DEFENSE_ID];
+        Building_BaseAtribut[BUILDING_GOLD_PER_TURN_ID]=400;
+        Building_BaseAtribut[BUILDING_BUILD_TIME_ID] = -1; // Agar pas tiap endturn dan ngecek tiap bangunan BUILD_TIME = 0 gak manggil method ini lagi bangunan yang dah manggil method ini
+    }
 }
--- DunnoTactic/src/Model/Building/Building.java
+++ DunnoTactic/src/Model/Building/Building.java
@@ -10,19 +10,30 @@ package Model.Building;
  * @author user
  */
 
-public class Building
+abstract public class Building
 {
-    protected  int[] Building_BaseAtribut = new int[Building_Constanta.NUMBER_BUILDING_ATRIBUT];
+    public static final int NUMBER_BUILDING_ATRIBUT = 5;
+    public static final int BUILDING_HP_ID = 0;
+    public static final int BUILDING_DEFENSE_ID = 1;
+    public static final int BUILDING_GOLD_PER_TURN_ID = 2;
+    public static final int BUILDING_BUILD_TIME_ID = 3;
+    public static final int BUILDING_LEVEL_ID =4;
+    public static final int BLACKSMITH_COST = 20000;
+    public static final int BARRACK_NOVICE_COST = 7500;
+    public static final int BARRACK_JOB_COST = 9000;
+
+    protected  int[] Building_BaseAtribut = new int[NUMBER_BUILDING_ATRIBUT];
 
     public Building()
     {
-
+    
     }
 
     /**
      * @return the Building_BaseAtribut
      */
-    public int getBuilding_BaseAtribut(int AtribBuildingID) {
+    public int getBuilding_BaseAtribut(int AtribBuildingID)
+    {
         return Building_BaseAtribut[AtribBuildingID];
     }
     
@@ -30,15 +41,73 @@ public class Building
      *
      * @param Building_BaseAtribut the Building_BaseAtribut to set
      */
-    public void setBuilding_BaseAtribut(int[] Building_BaseAtribut) {
+    public void setBuilding_BaseAtribut(int[] Building_BaseAtribut)
+    {
         this.Building_BaseAtribut = Building_BaseAtribut;
+    }
+
+    public void DecreaseBuildingBuildTime()
+    {
+        Building_BaseAtribut[BUILDING_BUILD_TIME_ID] = Building_BaseAtribut[BUILDING_BUILD_TIME_ID]-1;
+    }
+
+    // Masa membangun udah selesai, bangunan sempurna
+    abstract public void PerfectBuilding();
+   
+
+    public void getDamage(int Damage)
+    {
+        int DamageReceive;
+        DamageReceive = Damage-Building_BaseAtribut[BUILDING_DEFENSE_ID];
+        if (DamageReceive>0)
+        {
+            Building_BaseAtribut[BUILDING_HP_ID] = Building_BaseAtribut[BUILDING_HP_ID]-DamageReceive;
+        }
+        else
+        {
+            Building_BaseAtribut[BUILDING_HP_ID] = Building_BaseAtribut[BUILDING_HP_ID]-1;
+        }
     }
 
     public boolean IsBuildingDestroyed()
     {
-        return Building_BaseAtribut[Building_Constanta.BUILDING_HP_ID]==0;
+        return Building_BaseAtribut[BUILDING_HP_ID]<=0;
     }
 
+    public void UpgradeBuilding()
+    {
+       if (Building_BaseAtribut[BUILDING_LEVEL_ID]==1)
+       {
+           for (int i=0; i<NUMBER_BUILDING_ATRIBUT-1;++i)
+           {
+               Building_BaseAtribut[i]=(Building_BaseAtribut[i] + (int) Math.floor(0.1 * Building_BaseAtribut[i]));
+           }
+           Building_BaseAtribut[BUILDING_LEVEL_ID]=2;
+       }
+       else if (Building_BaseAtribut[BUILDING_LEVEL_ID]==2)
+       {
+           for (int i=0; i<NUMBER_BUILDING_ATRIBUT-1;++i)
+           {
+               Building_BaseAtribut[i]=(Building_BaseAtribut[i] + (int) Math.floor(0.3 * Building_BaseAtribut[i]));
+           }
+           Building_BaseAtribut[BUILDING_LEVEL_ID]=3;
+       }
+       else if (Building_BaseAtribut[BUILDING_LEVEL_ID]==3)
+       {
+           for (int i=0; i<NUMBER_BUILDING_ATRIBUT-1;++i)
+           {
+               Building_BaseAtribut[i]=(Building_BaseAtribut[i] + (int) Math.floor(0.3 * Building_BaseAtribut[i]));
+           }
+           Building_BaseAtribut[BUILDING_LEVEL_ID]=99;
+       }
+       else if (Building_BaseAtribut[BUILDING_LEVEL_ID]==99)
+       {
+           for (int i=0; i<NUMBER_BUILDING_ATRIBUT-1;++i)
+           {
+               Building_BaseAtribut[i]=(Building_BaseAtribut[i] + (int) Math.floor(0.4 * Building_BaseAtribut[i]));
+           }
+           Building_BaseAtribut[BUILDING_LEVEL_ID]=99;
+       }
+    }
     
-
 }
